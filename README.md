4db61bc3e3cb21505e97f9345e1e37a8e72de0e5
Ikpia <echukwuka97@gmail.com>
2023-02-16 01:18:31 +0100

    commit file

---
+++ README.md
@@ -0,0 +1,69 @@
+
+#  A Ecommerce Web Application
+
+An ecommerce web app is an application designed to facilitate online transactions between buyers and sellers. It typically includes a storefront that allows businesses to showcase their products, and a shopping cart that enables customers to select and purchase items.
+
+Ecommerce web apps can vary in complexity and functionality, depending on the needs of the business. A simple ecommerce web app may include basic features like a product catalog, shopping cart, and checkout process. More complex applications may offer additional features such as inventory management, shipping and tax calculations, and integration with payment gateways and shipping providers.
+
+In addition to these core features, an ecommerce web app may also include other tools and functionalities, such as customer account management, order history, wish lists, and product reviews. Many ecommerce web apps also integrate with other services and platforms, such as social media networks, email marketing tools, and analytics services, to help businesses reach and engage with their customers.
+
+## Features
+    
+ - See the products you plan to puchase displayed
+ - Add your chosen product to cart
+ - Calculate the selling price of the products in relation to the quantity demanded.
+ - Calculate the total price of all the product in the cart
+ 
+
+
+## Run Locally
+
+Clone the project
+
+```bash
+  git clone https://github.com/Ikpia/ecommerce_web_app
+```
+
+Go to the project directory
+
+```bash
+  cd ecommerce_web_app
+```
+
+Install dependencies
+
+```bash
+  npm install
+```
+
+Start the server
+
+```bash
+  node index.js
+
+```
+`make sure you have node js and mongodb are installed on your computer`
+
+
+## Authors
+
+- [Emmanuel](https://www.github.com/Ikpia)
+
+
+## Tech Stack
+
+**Client:** HTML, CSS, Javascript, Jquery, Bootstrap
+
+**Server:** Node, Express, ejs, mongoose
+
+**Database:** Mongodb
+
+
+## 🔗 Links
+[![linkedin](https://img.shields.io/badge/linkedin-0A66C2?style=for-the-badge&logo=linkedin&logoColor=white)](https://www.linkedin.com/in/emmanuel-ikpia-78428a236)
+[![twitter](https://img.shields.io/badge/twitter-1DA1F2?style=for-the-badge&logo=twitter&logoColor=white)](https://twitter.com/ikpia_e)
+
+
+## 🚀 About Me
+I'm a full stack developer...
+
---
+++ index.js
@@ -0,0 +1,251 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const session = require('express-session');
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://127.0.0.1:27017/ecomerceDB');
+const ejs = require('ejs');
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(session({
+    secret: 'secret key',
+    resave: false,
+    saveUninitialized: true
+  }));
+
+
+const itemSchema = new mongoose.Schema({
+    name : {
+        type: String,
+        required: true
+    },
+    description : {
+        type: String,
+        required: true
+    },
+    price : {
+        type: Number,
+        default: 0.0 
+    },
+
+    sales_price: Number,
+
+    quantity : {
+        type: Number,
+        default: 0
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    category: {
+        type: String,
+        required: true
+    },
+    type: {
+        type: String,
+        required: true
+    }
+});
+
+const Items = mongoose.model('Items', itemSchema);
+
+const cartSchema = new mongoose.Schema({
+    name : {
+        type: String,
+        required: true
+    },
+    price : {
+        type: Number,
+        default: 0.0 
+    },
+
+    sales_price: {
+        type: Number,
+        default: 0.0 
+    },
+
+    quantity : {
+        type: Number,
+        default: 0
+    },
+    image: {
+        type: String,
+        required: true,
+    },
+    subtotal : {
+        type: Number,
+        default: 0.0 
+    },
+    total : {
+        type: Number,
+        default: 0.0 
+    },
+});
+
+const CartProduct = mongoose.model('CartProduct', cartSchema);
+
+
+const item1 = {
+    name: 'Samsung s20',
+    description: 'samsung smartphone 32gb storage',
+    price: 399.0,
+    quantity: 5,
+    image: "images/1.png",
+    category: "smartphone",
+    type: "samsung"
+}
+const item2 = {
+    name: "Iphone 13",
+    description: "32gb 7",
+    price: 799.0,
+    sales_price: 699.0,
+    quantity:10,
+    image: "images/2.png",
+    category: "smartphone",
+    type: "iphone"
+}
+const item3 = {
+    name: "Iphone 14",
+    description: "32gb 8",
+    price: 899.0,
+    sales_price: 799.0,
+    quantity:10,
+    image: "images/3.png",
+    category: "smartphone",
+    type: "iphone"
+}
+const item4 = {
+    name: "Samsung",
+    description: "Samsung 8 32gb",
+    price: 799.0,
+    sales_price: 699.0,
+    quantity: 10,
+    image: "images/4.png",
+    category: "smartphone",
+    type: "Samsung"
+}
+const item5 = {
+    name: "Panasonic 9",
+    description: "Panasonic 9 32gb",
+    price: 899.0,
+    sales_price: 699.0,
+    quantity: 10,
+    image: "images/5.png",
+    category: "smartphone",
+    type: "Panasonic"
+}
+const item6 = {
+    name: "ig 8",
+    description: "ig 8 32gb",
+    price: 799.0,
+    sales_price: 699.0,
+    quantity: 10,
+    image: "images/6.png",
+    category: "smartphone",
+    type: "Ig"
+}
+
+var theItems = [item1, item2, item3, item4, item5, item6];
+
+app.get('/', (req, res) => {
+    Items.find({}, (err, docs) => {
+        if (docs.length == 0) {
+            Items.insertMany(theItems, (err) => {
+                if (!err) {
+                  res.redirect('/');
+                } else {
+                    console.log(err);
+                }
+              });
+          
+        } else {
+            res.render('./pages/index', {allItems:docs});
+            //console.log(docs);
+        }
+      })
+    console.log(req.session.cart)
+    
+})
+
+
+app.post('/add_to_cart', (req, res) => {
+    var Product = new CartProduct({
+    name: req.body.name,
+    price: req.body.price,
+    sales_price: req.body.sales_price,
+    quantity: req.body.quantity,
+    image: req.body.image
+    });
+    console.log(req.body);
+    CartProduct.findOne({image: Product.image}, (err, doc) => {
+        if (!doc) {
+            Product.save();
+            res.redirect('/cart')
+        }
+    })
+})
+
+app.post('/cart_product', (req, res) => {
+    var submit = req.body.submit;
+    var price =  parseInt(req.body.price);
+    var total = req.body.total;
+    var name = req.body.name;
+    var quantity =  parseInt(req.body.quantity);
+    var sub_total = req.body.sub_total;
+    if (submit == 'add') {
+        quantity = quantity + 1;
+        
+    } else if (submit == 'subtract') {
+        quantity = quantity - 1;
+    } 
+    sub_total = quantity * price;
+    console.log(name)
+    CartProduct.findOneAndUpdate({name: name}, { quantity:quantity, subtotal:sub_total },{ new: true },(err, results) =>{
+        if (err) {
+            console.log(err);
+        }
+        res.redirect('/cart');
+    })
+
+    
+})
+
+app.post('/delete_cart', (req, res) => {
+    var submit = req.body.submit;
+    var name = req.body.name;
+
+    if (submit == 'remove') {
+        CartProduct.findOneAndDelete({name: name}, (err, results) => {
+            if (err) {
+                console.log(err)
+            } else {
+              res.redirect('/cart');
+            }
+        })
+    }
+})
+
+app.get('/cart', (req, res) => {
+    CartProduct.find({}, (err, results) =>{
+        var subTotalArray = [];
+        var sum = 0;
+        for (var i = 0; i < results.length; i++) {
+            subTotalArray.push(results[i].subtotal);
+        }
+        for (let i = 0; i < subTotalArray.length; i++) {  
+            sum += subTotalArray[i];
+          }
+        if (err) {
+            console.log(err);
+        } else {
+            res.render('./pages/cart', {theProduct:results, total:sum});
+        }
+    })
+    
+})
+
+app.listen(3000, () => {
+    console.log('Server up and running');
+})
